869af2a877c09cb56916479160f5e6ad424a84a9
Tareq Hassan <tareqhassan2014.com>
2022-10-20 15:34:10 +0600

    add Facebook messenger

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import Box from '@mui/material/Box';
 import { ThemeProvider } from '@mui/material/styles';
 import React from 'react';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import { USER_ENTRY_FORM_TYPE } from './common/constant';
 import Footer from './components/Footer';
@@ -47,91 +48,109 @@ function App() {
     };
 
     return (
-        <ThemeProvider theme={theme}>
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-                <div
-                    style={{
-                        minHeight: '100vh',
-                        display: 'flex',
-                        flexDirection: 'column',
-                    }}
-                >
-                    <Header />
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/contact" element={<ContactUs />} />
-                        <Route
-                            path="/login"
-                            element={
-                                <Box marginTop="60px">
-                                    <LoginForm
-                                        onChangeFormType={handleFormTypeChange}
-                                    />{' '}
-                                </Box>
-                            }
-                        />
-                        <Route
-                            path="/signup"
-                            element={
-                                <Box marginTop="60px">
-                                    <SignUpForm
-                                        onChangeFormType={handleFormTypeChange}
-                                    />
-                                </Box>
-                            }
-                        />
-                        <Route
-                            path="/forgot"
-                            element={
-                                <Box marginTop="60px">
-                                    <ForgotPassword onSubmit={() => {}} />{' '}
-                                </Box>
-                            }
-                        />
+        <>
+            <MessengerCustomerChat
+                pageId={process.env.REACT_APP_FACEBOOK_PAGE_ID}
+                appId={process.env.REACT_APP_FACEBOOK_APP_ID}
+            />
+            <ThemeProvider theme={theme}>
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                    <div
+                        style={{
+                            minHeight: '100vh',
+                            display: 'flex',
+                            flexDirection: 'column',
+                        }}
+                    >
+                        <Header />
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/contact" element={<ContactUs />} />
+                            <Route
+                                path="/login"
+                                element={
+                                    <Box marginTop="60px">
+                                        <LoginForm
+                                            onChangeFormType={
+                                                handleFormTypeChange
+                                            }
+                                        />{' '}
+                                    </Box>
+                                }
+                            />
+                            <Route
+                                path="/signup"
+                                element={
+                                    <Box marginTop="60px">
+                                        <SignUpForm
+                                            onChangeFormType={
+                                                handleFormTypeChange
+                                            }
+                                        />
+                                    </Box>
+                                }
+                            />
+                            <Route
+                                path="/forgot"
+                                element={
+                                    <Box marginTop="60px">
+                                        <ForgotPassword onSubmit={() => {}} />{' '}
+                                    </Box>
+                                }
+                            />
 
-                        <Route path="/booking" element={<Booking />}>
-                            <Route
-                                index
-                                element={<Redirect to="/booking/reservation" />}
-                            />
-                            <Route
-                                path="reservation"
-                                element={<BookingVehicles />}
-                            />
-                            <Route
-                                path="upcoming-ride"
-                                element={<UpcomingRide />}
-                            />
-                            <Route path="past-ride" element={<PastRide />} />
-                            <Route
-                                path="quote-requests"
-                                element={<QuoteRequests />}
-                            />
-                            <Route
-                                path="payment-methods"
-                                element={<PaymentMethods />}
-                            />
-                            <Route path="my-account" element={<MyAccount />} />
+                            <Route path="/booking" element={<Booking />}>
+                                <Route
+                                    index
+                                    element={
+                                        <Redirect to="/booking/reservation" />
+                                    }
+                                />
+                                <Route
+                                    path="reservation"
+                                    element={<BookingVehicles />}
+                                />
+                                <Route
+                                    path="upcoming-ride"
+                                    element={<UpcomingRide />}
+                                />
+                                <Route
+                                    path="past-ride"
+                                    element={<PastRide />}
+                                />
+                                <Route
+                                    path="quote-requests"
+                                    element={<QuoteRequests />}
+                                />
+                                <Route
+                                    path="payment-methods"
+                                    element={<PaymentMethods />}
+                                />
+                                <Route
+                                    path="my-account"
+                                    element={<MyAccount />}
+                                />
 
-                            <Route path="blank" element={<BlankPage />} />
-                        </Route>
-                        <Route
-                            path="/conditions"
-                            element={<TermsAndConditions />}
-                        />
-                        <Route
-                            path="/ReurnAndRefunds"
-                            element={<ReurnAndRefunds />}
-                        />
-                        <Route
-                            path="/PrivacyPolicy"
-                            element={<PrivacyPolicy />}
-                        />
-                    </Routes>
-                    <Footer />
-                </div>
-            </LocalizationProvider>
-        </ThemeProvider>
+                                <Route path="blank" element={<BlankPage />} />
+                            </Route>
+                            <Route
+                                path="/conditions"
+                                element={<TermsAndConditions />}
+                            />
+                            <Route
+                                path="/ReurnAndRefunds"
+                                element={<ReurnAndRefunds />}
+                            />
+                            <Route
+                                path="/PrivacyPolicy"
+                                element={<PrivacyPolicy />}
+                            />
+                        </Routes>
+                        <Footer />
+                    </div>
+                </LocalizationProvider>
+            </ThemeProvider>
+        </>
     );
 }
 
